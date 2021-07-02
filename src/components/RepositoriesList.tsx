c543f7bf870048e1ebce8a713255e51ef2fedb71
Oleh <orenkole@gmail.com>
2021-07-02 21:48:40 +0300

    39. Binding Action Creators

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,15 +1,18 @@
 import React, {useState} from 'react';
-import { useDispatch } from 'react-redux';
-import { actionCreators } from '../state';
+import { useActions } from '../hooks/useActions';
+// import { useDispatch } from 'react-redux';
+// import { actionCreators } from '../state';
 
 
 const RepositoriesList: React.FC = () => {
   const [term, setTerm] = useState("")
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
+  const {searchRepositories} = useActions();
 
   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    dispatch(actionCreators.searchRepositories(term));
+    // dispatch(actionCreators.searchRepositories(term));
+    searchRepositories(term);
   }
 
   return (
